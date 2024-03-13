48bd7be1eee716a646b814cfce96c37b30838656
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-03-13 18:13:34 +0100

    add full page scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import YGWYS from "@/components/YGWYS/YGWYS";
+import Page from "@/components/Page/Page";
 import { performRequest } from "@/datocms";
 import { HomePagina } from "@/models/homePagina";
-import { render as dastRender } from 'datocms-structured-text-to-dom-nodes';
 
 const PAGE_CONTENT_QUERY = `
 query MyQuery {
@@ -25,25 +24,9 @@ query MyQuery {
 export default async function Home() {
   const { data: { homePagina } } = await performRequest<{ data: { homePagina: HomePagina } }>({query: PAGE_CONTENT_QUERY})
 
-  const nodes = dastRender(homePagina.infoTekst.value)
-
   return (
     <main>
-      <header className='bg-no-repeat bg-cover bg-center md:pt-96' style={{ backgroundImage: `url(${homePagina.achtergrond.url})` }}>
-        <h1 className="md:text-6xl text-4xl pt-96 pb-48 md:pl-40 pl-10 text-white">{homePagina.titel}</h1>
-      </header>
-      
-      <section>
-        <article className="mt-20 mb-20 md:pl-40 pl-10 pr-10 md:w-1/2 text-xl">
-          {nodes?.map(node => <YGWYS key={node.id} html={node.outerHTML} />)} 
-        </article>
-      </section>
-
-      <footer className="flex flex-col text-center">
-        <p>{homePagina.adres}</p>
-        <p>{homePagina.telefoonNummer}</p>
-      </footer>
-
+      <Page homePagina={homePagina} />
     </main>
   );
 }
---
+++ components/Page/Page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { HomePagina } from '@/models/homePagina';
+import ReactPageScroller from 'react-page-scroller';
+import YGWYS from "@/components/YGWYS/YGWYS";
+import { render as dastRender } from 'datocms-structured-text-to-dom-nodes';
+import getUniqueId from '@/helpers/getUniqueId';
+
+const Page = ({homePagina}: {homePagina: HomePagina}) => {
+    const nodes = dastRender(homePagina.infoTekst.value)
+
+    const error = console.error;
+    console.error = (...args: any) => {
+        if (/defaultProps/.test(args[0])) return;
+        error(...args);
+    };
+
+
+    return (
+        <ReactPageScroller>
+            <header className='bg-no-repeat bg-cover bg-center md:pt-96' style={{ backgroundImage: `url(${homePagina.achtergrond.url})` }}>
+              <h1 className="md:text-6xl text-4xl pt-96 pb-48 md:pl-40 pl-10 text-white">{homePagina.titel}</h1>
+            </header>
+
+            <section>
+              <article className="mt-20 mb-20 md:pl-40 pl-10 pr-10 md:w-1/2 text-xl">
+                {nodes?.map(node => <YGWYS key={getUniqueId()} html={node.outerHTML} />)} 
+              </article>
+            </section>
+
+            <footer className="flex flex-col text-center">
+              <p>{homePagina.adres}</p>
+              <p>{homePagina.telefoonNummer}</p>
+            </footer>
+        </ReactPageScroller>
+    )
+}
+
+export default Page
