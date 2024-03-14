9810033263d9d58cc14f6faff3c8175edf766a78
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-03-14 13:59:51 +0100

    full screen image

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -14,23 +14,24 @@ const Page = ({homePagina}: {homePagina: HomePagina}) => {
         error(...args);
     };
 
-
     return (
         <ReactPageScroller>
-            <header className='bg-no-repeat bg-cover bg-center md:pt-96' style={{ backgroundImage: `url(${homePagina.achtergrond.url})` }}>
-              <h1 className="md:text-6xl text-4xl pt-96 pb-48 md:pl-40 pl-10 text-white">{homePagina.titel}</h1>
+          <section>
+            <header className='bg-no-repeat bg-cover bg-center md:pt-96 min-h-screen' style={{ backgroundImage: `url(${homePagina.achtergrond.url})` }}>
+              <h1 className="md:text-6xl text-4xl md:w-1/2 pt-96 pb-48 md:pl-40 pl-10 text-white">{homePagina.titel}</h1>
             </header>
+          </section>
 
-            <section>
-              <article className="mt-20 mb-20 md:pl-40 pl-10 pr-10 md:w-1/2 text-xl">
-                {nodes?.map(node => <YGWYS key={getUniqueId()} html={node.outerHTML} />)} 
-              </article>
-            </section>
+          <section>
+            <article className="mt-20 mb-20 md:pl-40 pl-10 pr-10 md:w-1/2 text-xl">
+              {nodes?.map(node => <YGWYS key={getUniqueId()} html={node.outerHTML} />)} 
+            </article>
+          </section>
 
-            <footer className="flex flex-col text-center">
-              <p>{homePagina.adres}</p>
-              <p>{homePagina.telefoonNummer}</p>
-            </footer>
+          <footer className="flex flex-col text-center">
+            <p>{homePagina.adres}</p>
+            <p>{homePagina.telefoonNummer}</p>
+          </footer>
         </ReactPageScroller>
     )
 }
