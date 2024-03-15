1627b75622a2a7a39cf609e1ec8024a279728e1d
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-03-15 10:39:47 +0100

    separate component

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = ({ handleNavBarClick }: { handleNavBarClick: (index: number) => void; }) => (
+    <nav className='bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200'>
+        <ul className='flex items-center justify-center mx-auto p-4'>
+            <li onClick={() => handleNavBarClick(0)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>Home</li>
+            <li onClick={() => handleNavBarClick(1)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>Welkom</li>
+            <li onClick={() => handleNavBarClick(2)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>Kalender</li>
+            <li onClick={() => handleNavBarClick(3)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>contact</li>
+        </ul>
+  </nav>
+)
+
+export default NavBar
--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -4,8 +4,8 @@ import ReactPageScroller from 'react-page-scroller';
 import YGWYS from '@/components/YGWYS/YGWYS';
 import { render as dastRender } from 'datocms-structured-text-to-dom-nodes';
 import getUniqueId from '@/helpers/getUniqueId';
-import Link from 'next/link';
 import { useState } from 'react';
+import NavBar from '../NavBar/NavBar';
 
 const Page = ({homePagina}: {homePagina: HomePagina}) => {
   const [currentPageNumber, setCurrentPageNumber] = useState(0)
@@ -31,14 +31,7 @@ const Page = ({homePagina}: {homePagina: HomePagina}) => {
 
   return (
     <>
-      <nav className='bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200'>
-        <ul className='flex items-center justify-center mx-auto p-4'>
-          <li onClick={() => handleNavBarClick(0)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>Home</li>
-          <li onClick={() => handleNavBarClick(1)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>Welkom</li>
-          <li onClick={() => handleNavBarClick(2)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>Kalender</li>
-          <li onClick={() => handleNavBarClick(3)} className='py-2 px-3 hover:text-blue-700 cursor-pointer'>contact</li>
-        </ul>
-      </nav>
+      <NavBar handleNavBarClick={handleNavBarClick} />
 
       <ReactPageScroller
         customPageNumber={currentPageNumber}
@@ -70,7 +63,7 @@ const Page = ({homePagina}: {homePagina: HomePagina}) => {
         <section
           className={`flex flex-col text-center text-xl ${section}`}
           id="contact"
-          >
+        >
           <p className='pt-20 text-xl'>{homePagina.naam}</p>
           <p className='text-xl'>{homePagina.email}</p>
           <p className='text-xl'>{homePagina.telefoonNummer}</p>            
