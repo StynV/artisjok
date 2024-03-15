f29a8d18b175b69e9cc58743ef42a5f42c76ae5c
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-03-15 16:15:38 +0100

    seo van cms halen

---
+++ models/layout.ts
@@ -0,0 +1,15 @@
+export interface Attributes {
+    name?: string,
+    property?: string,
+    content: string
+}
+
+export interface MetaTag {
+    attributes: Attributes | null,
+    content: string | null,
+    tag: string
+}
+
+export interface Layout {
+    "_seoMetaTags": MetaTag[]
+}
