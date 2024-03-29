75d9e4482cb8528476cd76d1a0d571f0d5b369b6
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-03-29 11:45:17 +0100

    update color navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 const NavBar = ({ handleNavBarClick }: { handleNavBarClick: (index: number) => void; }) => (
-    <nav className='bg-white bg-opacity-50 fixed w-full z-20 top-0 start-0'>
+    <nav className='bg-gray-200 bg-opacity-50 fixed w-full z-20 top-0 start-0'>
         <ul className='flex items-center justify-center mx-auto p-4'>
             <li onClick={() => handleNavBarClick(0)} className='py-2 px-3 text-black md:text-2xl hover:text-blue-700 cursor-pointer'>Home</li>
             <li onClick={() => handleNavBarClick(1)} className='py-2 px-3 text-black md:text-2xl hover:text-blue-700 cursor-pointer'>IAM</li>
