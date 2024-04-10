7b73f069020460ec9eb453ff32e03195ceea40f9
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-04-10 10:44:11 +0200

    move method to component

--- components/KalenderItem/KalenderItem.tsx
+++ components/KalenderItem/KalenderItem.tsx
@@ -1,9 +1,20 @@
 import { AllKalenders } from "@/models/allKalenders"
 import Link from "next/link"
 import Image from 'next/image';
-import getRandomColor from "@/helpers/getRandomColor";
 
 const KalenderItem = ({ kalenderItem }: { kalenderItem: AllKalenders | undefined }) => {
+  const getRandomColor = () => {
+    const colors = [
+        'bg-pink-400',
+        'bg-cyan-200',
+        'bg-emerald-300',
+        'bg-yellow-300'
+    ]
+    
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex]
+}
+
   if (kalenderItem && kalenderItem?.id !== '') {
     return (
       <Link
