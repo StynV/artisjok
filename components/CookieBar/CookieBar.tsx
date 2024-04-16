d0b1f9be423aed06958b8500b244c28cfce5ee0c
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2024-04-16 20:06:32 +0200

    visibility fixes

--- components/CookieBar/CookieBar.tsx
+++ components/CookieBar/CookieBar.tsx
@@ -26,16 +26,18 @@ const CookieBar = () => {
 
   return (
     <div className="fixed inset-0 bg-slate-700 bg-opacity-70" onClick={notAcceptCookie}>
-      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between px-4 py-8 bg-gray-100">
-        <span className="text-dark text-base mr-16">
+      <div className="fixed bottom-0 left-0 right-0 flex md:flex-row flex-col items-center justify-between px-4 py-8 bg-gray-100">
+        <span className="text-black text-base mr-16 mb-5 md:mb-0">
           Deze website gebruikt cookies om de user experience hoog te houden. Door onze website te gebruiken kom je hiermee overeen.
         </span>
-        <button className="bg-green-500 py-2 px-8 rounded text-white mr-5" onClick={() => acceptCookie()}>
-          Accepteer
-        </button>
-        <button className="bg-green-500 py-2 px-8 rounded text-white" onClick={() => notAcceptCookie()}>
-          Weiger
-        </button>
+        <div>
+          <button className="bg-green-500 py-2 px-8 rounded text-white mr-5" onClick={() => acceptCookie()}>
+            Accepteer
+          </button>
+          <button className="bg-green-500 py-2 px-8 rounded text-white mr-5" onClick={() => notAcceptCookie()}>
+            Weiger
+          </button>
+        </div>
       </div>
     </div>
   );
