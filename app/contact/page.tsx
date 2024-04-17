08599aac6db8385de587b36f8f2e94f9d8d2427c
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2024-04-17 21:12:49 +0200

    add footer

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import Icon from "@/components/Icon/Icon";
 import { performRequest } from "@/datocms/performRequest";
 import { PAGE_CONTENT_QUERY } from "@/datocms/queries";
@@ -9,37 +10,41 @@ export default async function Contact() {
     const { data: { homePagina } } = await performRequest<{ data: { homePagina: HomePagina } }>({ query: PAGE_CONTENT_QUERY })
 
     return (
-        <section
-            className={'flex flex-col items-center justify-center text-xl bg-white min-h-screen lg:pr-40 pr-10 lg:pl-40 pl-10'}
-        >
-            <Image
-                src={homePagina.iamLogo.url}
-                alt={homePagina.iamLogo.alt}
-                height={homePagina.iamLogo.height}
-                width={homePagina.iamLogo.width}
-                priority
-            />
+        <>
+            <section
+                className={'flex flex-col items-center justify-center text-xl bg-white min-h-screen lg:pr-40 pr-10 lg:pl-40 pl-10'}
+            >
+                <Image
+                    src={homePagina.iamLogo.url}
+                    alt={homePagina.iamLogo.alt}
+                    height={homePagina.iamLogo.height}
+                    width={homePagina.iamLogo.width}
+                    priority
+                />
 
-            <div className='flex flex-row gap-5 items-center'>
-                <Icon href={homePagina.facebook} src={homePagina.facebookLogo.url} alt={homePagina.facebookLogo.alt} />
-                <Icon href={homePagina.instagram} src={homePagina.instagramLogo.url} alt={homePagina.instagramLogo.alt} />
-                <Icon href={homePagina.linkedin} src={homePagina.linkedinLogo.url} alt={homePagina.linkedinLogo.alt} />
-            </div>
-
-            <div className='flex flex-col items-center pt-10'>
-                <Link
-                    href={`mailto:${homePagina.email}`}
-                    className='text-xl text-black'
-                >
-                    {homePagina.email}
-                </Link>
-                <div
-                    className='text-center text-xl text-black'
-                >
-                    <p>{homePagina.straat} {homePagina.huisnummer}</p>
-                    <p>{homePagina.gemeente} {homePagina.postcode}</p>
+                <div className='flex flex-row gap-5 items-center'>
+                    <Icon href={homePagina.facebook} src={homePagina.facebookLogo.url} alt={homePagina.facebookLogo.alt} />
+                    <Icon href={homePagina.instagram} src={homePagina.instagramLogo.url} alt={homePagina.instagramLogo.alt} />
+                    <Icon href={homePagina.linkedin} src={homePagina.linkedinLogo.url} alt={homePagina.linkedinLogo.alt} />
                 </div>
-            </div>
-        </section>
+
+                <div className='flex flex-col items-center pt-10'>
+                    <Link
+                        href={`mailto:${homePagina.email}`}
+                        className='text-xl text-black'
+                    >
+                        {homePagina.email}
+                    </Link>
+                    <div
+                        className='text-center text-xl text-black'
+                    >
+                        <p>{homePagina.straat} {homePagina.huisnummer}</p>
+                        <p>{homePagina.gemeente} {homePagina.postcode}</p>
+                    </div>
+                </div>
+            </section>
+
+            <Footer />
+        </>
     )
 }
---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className='fixed inset-0'>
+            <div className="fixed bottom-0 left-0 right-0 flex md:flex-row flex-col items-center justify-between px-4 py-8 bg-gray-100">
+                <div className='text-black md:text-2xl flex items-center justify-center w-full'>
+                    <p className="pr-1">Copyright {currentYear} - Independent Art Magazine -</p><Link className="hover:text-blue-700 cursor-pointer" target="_blank" href='https://cv-styn-vercauteren.vercel.app/'>Site by Styn Vercauteren</Link>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
