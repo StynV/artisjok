c0babacf6f5f10815a084fdd7c723018db2c3c48
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2024-04-20 11:11:05 +0200

    open socials in new tab

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import Image from 'next/image';
 
 const Icon = ({ href, src, alt }: { href: string, src: string, alt: string }) => (
-    <Link href={href}>
+    <Link href={href} target="_blank">
         <Image
             src={src}
             alt={alt}
