ab225a533d42a355f6f60514df02eea36c5f4f65
Styn Vercauteren <styn.vercauteren@two-point-o.be>
2024-06-15 11:57:55 +0200

    cover bigger on mobile

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,109 +1,106 @@
-import { useFeedbackSubmittedContext } from "@/context/feedback"
-import { postFeedbackRequest } from "@/datocms/postFeedbackRequest"
-import { useState } from "react"
-import emailjs from 'emailjs-com';
+import { useFeedbackSubmittedContext } from "@/context/feedback";
+import { postFeedbackRequest } from "@/datocms/postFeedbackRequest";
+import { useState } from "react";
+import emailjs from "emailjs-com";
 
 const Form = () => {
-    const [showThanks, setShowThanks] = useState(false)
+  const [showThanks, setShowThanks] = useState(false);
 
-    const { reSetchSubmittedFeedbacks } = useFeedbackSubmittedContext()
+  const { reSetchSubmittedFeedbacks } = useFeedbackSubmittedContext();
 
-    const [formData, setFormData] = useState({
-        naam: "",
-        opmerking: "",
-        likes: 0
-    })
+  const [formData, setFormData] = useState({
+    naam: "",
+    opmerking: "",
+    likes: 0,
+  });
 
-    const handleInput = (e: any) => {
-        const fieldName = e.target.name
-        const fieldValue = e.target.value
+  const handleInput = (e: any) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
 
-        setFormData((prevState) => ({
-            ...prevState,
-            [fieldName]: fieldValue
-        }))
+    setFormData((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue,
+    }));
+  };
+
+  const submitForm = async (e: any) => {
+    e.preventDefault();
+
+    setShowThanks(true);
+    setInterval(() => setShowThanks(false), 5000);
+
+    setFormData({
+      naam: "",
+      opmerking: "",
+      likes: 0,
+    });
+
+    const response = await postFeedbackRequest({
+      naam: formData.naam,
+      opmerking: formData.opmerking,
+      likes: formData.likes,
+    });
+
+    if (response) {
+      reSetchSubmittedFeedbacks();
+
+      const templateParams = {
+        to_name: "independentartmagazine@gmail.com",
+        from_name: "independentartmagazine@gmail.com",
+        form_name: formData.naam,
+        message: formData.opmerking,
+      };
+
+      // emailjs.send('service_ux0vih8', 'template_ppfwqkl', templateParams, 'yZ0JwXzFm2XsODp6Q')
     }
+  };
 
-    const submitForm = async (e: any) => {
-        e.preventDefault()
+  return (
+    <form onSubmit={submitForm} className="md:mr-10 mb-4">
+      {showThanks && (
+        <div className="bg-green-500 p-4 mb-4 text-white">
+          <p>Dankjewel voor jouw feedback.</p>
+        </div>
+      )}
 
-        setShowThanks(true)
-        setInterval(() => setShowThanks(false), 5000)
+      <div className="flex flex-col mb-2">
+        <label className="mb-2 flex flex-row">
+          <p className="text-black">Naam</p>
+          <p className="text-red-600">*</p>
+        </label>
+        <input
+          type="text"
+          name="naam"
+          onChange={handleInput}
+          value={formData.naam}
+          required
+          className="border border-slate-300 mb-2 rounded-md p-2 text-black"
+        />
+      </div>
 
-        setFormData({
-            naam: "",
-            opmerking: "",
-            likes: 0
-        })
+      <div className="flex flex-col mb-2">
+        <label className="mb-2 flex flex-row">
+          <p className="text-black">Opmerking</p>
+          <p className="text-red-600">*</p>
+        </label>
+        <textarea
+          name="opmerking"
+          onChange={handleInput}
+          value={formData.opmerking}
+          required
+          className="border border-slate-300 mb-2 rounded-md xl:p-2 lg:p-0 md:p-2 text-black"
+        />
+      </div>
 
-        const response = await postFeedbackRequest(
-            {
-                naam: formData.naam,
-                opmerking: formData.opmerking,
-                likes: formData.likes
-            }
-        )
+      <button
+        type="submit"
+        className="bg-blue-500 text-white md:py-2 py-1 w-full rounded-md"
+      >
+        Verzend
+      </button>
+    </form>
+  );
+};
 
-        if (response) {
-            reSetchSubmittedFeedbacks()
-
-            const templateParams = {
-                to_name: 'independentartmagazine@gmail.com',
-                from_name: 'independentartmagazine@gmail.com',
-                form_name: formData.naam,
-                message: formData.opmerking
-            }
-
-            emailjs.send('service_ux0vih8', 'template_ppfwqkl', templateParams, 'yZ0JwXzFm2XsODp6Q')
-        }
-    }
-
-    return (
-        <form
-            onSubmit={submitForm}
-            className="md:mr-10 mb-4"
-        >
-            {showThanks && <div className="bg-green-500 p-4 mb-4 text-white">
-                <p>Dankjewel voor jouw feedback.</p>
-            </div>}
-
-            <div className="flex flex-col mb-2">
-                <label className="mb-2 flex flex-row">
-                    <p className="text-black">Naam</p>
-                    <p className="text-red-600">*</p>
-                </label>
-                <input
-                    type="text"
-                    name="naam"
-                    onChange={handleInput}
-                    value={formData.naam}
-                    required
-                    className="border border-slate-300 mb-2 rounded-md p-2 text-black"
-                />
-            </div>
-
-            <div className="flex flex-col mb-2">
-                <label className="mb-2 flex flex-row">
-                    <p className="text-black">Opmerking</p>
-                    <p className="text-red-600">*</p>
-                </label>
-                <textarea
-                    name="opmerking"
-                    onChange={handleInput}
-                    value={formData.opmerking}
-                    required
-                    className="border border-slate-300 mb-2 rounded-md xl:p-2 lg:p-0 md:p-2 text-black"
-                />
-            </div>
-
-            <button
-                type="submit"
-                className="bg-blue-500 text-white md:py-2 py-1 w-full rounded-md"
-            >
-                Verzend
-            </button>
-        </form>
-    )
-}
-
-export default Form
+export default Form;
--- components/Pages/IAM.tsx
+++ components/Pages/IAM.tsx
@@ -1,163 +1,176 @@
-'use client'
+"use client";
 
-import Form from '../Form/Form';
-import { Feedback } from '@/models/feedback';
-import { Image as ImageModel } from '@/models/image';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import LikeButton from '../LikeButton/LikeButton';
-import { useFeedbackSubmittedContext } from '@/context/feedback';
-import { performRequest } from '@/datocms/performRequest';
-import { FEEDBACK_QUERY } from '@/datocms/queries';
+import Form from "../Form/Form";
+import { Feedback } from "@/models/feedback";
+import { Image as ImageModel } from "@/models/image";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import LikeButton from "../LikeButton/LikeButton";
+import { useFeedbackSubmittedContext } from "@/context/feedback";
+import { performRequest } from "@/datocms/performRequest";
+import { FEEDBACK_QUERY } from "@/datocms/queries";
 
-const IAM = ({ title, covers }: { title: string, covers: ImageModel[] }) => {
-    const containerRef = useRef<HTMLDivElement>(null)
+const IAM = ({ title, covers }: { title: string; covers: ImageModel[] }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
 
-    const [scrollAtStart, setScrollAtStart] = useState(true)
-    const [scrollAtEnd, setScrollAtEnd] = useState(false)
+  const [scrollAtStart, setScrollAtStart] = useState(true);
+  const [scrollAtEnd, setScrollAtEnd] = useState(false);
 
-    const isMobile = useMediaQuery({ maxWidth: 768 })
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
-    const [allFeedbacks, setAllFeedbacks] = useState<Feedback[]>([])
-    const { feedbackSubmitted } = useFeedbackSubmittedContext()
+  const [allFeedbacks, setAllFeedbacks] = useState<Feedback[]>([]);
+  const { feedbackSubmitted } = useFeedbackSubmittedContext();
 
-    useEffect(() => {
-        async function fetchFeedbacks() {
-            const { data: { allFeedbacks } } = await performRequest<{ data: { allFeedbacks: Feedback[] } }>({ query: FEEDBACK_QUERY })
-            setAllFeedbacks(allFeedbacks)
-        }
-
-        fetchFeedbacks()
-    }, [feedbackSubmitted])
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (containerRef.current) {
-                setScrollAtStart(containerRef.current.scrollLeft === 0)
-                setScrollAtEnd(
-                    containerRef.current.scrollLeft + containerRef.current.clientWidth ===
-                    containerRef.current.scrollWidth
-                );
-            }
-        }
-
-        containerRef.current?.addEventListener('scroll', handleScroll)
-
-        return () => {
-            containerRef.current?.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
-
-    const handleBackClick = () => {
-        if (containerRef.current) {
-            if (isMobile) {
-                containerRef.current.scrollBy({
-                    left: -150,
-                    behavior: 'smooth'
-                })
-            } else {
-                containerRef.current.scrollBy({
-                    left: -350,
-                    behavior: 'smooth'
-                })
-            }
-        }
+  useEffect(() => {
+    async function fetchFeedbacks() {
+      const {
+        data: { allFeedbacks },
+      } = await performRequest<{ data: { allFeedbacks: Feedback[] } }>({
+        query: FEEDBACK_QUERY,
+      });
+      setAllFeedbacks(allFeedbacks);
     }
 
-    const handleNextClick = () => {
-        if (containerRef.current) {
-            if (isMobile) {
-                containerRef.current.scrollBy({
-                    left: 150,
-                    behavior: 'smooth'
-                })
-            } else {
-                containerRef.current.scrollBy({
-                    left: 350,
-                    behavior: 'smooth'
-                })
-            }
-        }
-    }
+    fetchFeedbacks();
+  }, [feedbackSubmitted]);
 
-    const handleWheel = (event: any) => {
-        const div = containerRef.current;
-
-        if (div && event.deltaX !== 0 && Math.abs(event.deltaX) > Math.abs(event.deltaY)) {
-            event.stopPropagation();
-        }
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        setScrollAtStart(containerRef.current.scrollLeft === 0);
+        setScrollAtEnd(
+          containerRef.current.scrollLeft + containerRef.current.clientWidth ===
+            containerRef.current.scrollWidth
+        );
+      }
     };
 
-    const btnStyling = 'p-2 pl-4 pr-4 border-gray-500 border-2 rounded-full w-10 h-10 flex justify-center items-center'
+    containerRef.current?.addEventListener("scroll", handleScroll);
 
-    return (
-        <section className='min-h-screen bg-white pt-28 md:pl-40 pl-10 md:pr-40 pr-10 pb-28'>
-            <section className='mb-4'>
-                <h1 className='md:text-9xl lg:text-4xl text-2xl text-center md:mb-10 mb-4 text-black'>{title}</h1>
-                <div className='wrapper flex justify-center mb-2'>
-                    <div
-                        className='flex flex-row justify-start gap-10 overflow-x-scroll overflow-y-hidden'
-                        style={{ width: `${(80 * 17) + (10 * 3)}px` }}
-                        ref={containerRef}
-                        onWheel={handleWheel}
-                    >
-                        {covers.map((image, index) => (
-                            <Link key={index} href={image.title ?? ''}>
-                                <div className="lg:w-36 xl:w-80 w-24">
-                                    <Image
-                                        src={image.url}
-                                        alt={image.alt}
-                                        height={image.height}
-                                        width={image.width}
-                                        priority
-                                    />
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-                <div className='flex justify-end pr-4'>
-                    <button
-                        className={`${btnStyling} mr-2 ${scrollAtStart && 'border-gray-100 text-gray-200'}`}
-                        onClick={handleBackClick}
-                        disabled={scrollAtStart}
-                    >
-                        <p className='text-black'>{`<`}</p>
-                    </button>
-                    <button
-                        className={`${btnStyling} ${scrollAtEnd && 'border-gray-100 text-gray-200'}`}
-                        onClick={handleNextClick}
-                        disabled={scrollAtEnd}
-                    >
-                        <p className='text-black'>{`>`}</p>
-                    </button>
-                </div>
-            </section>
-            <section className='lg:grid lg:grid-cols-3 xl:ml-40 xl:pr-40'>
-                <section className='lg:col-span-1'>
-                    <article>
-                        <Form />
-                    </article>
-                </section>
-                <section className='lg:col-span-2 lg:grid lg:grid-cols-2 overflow-y-scroll h-60'>
-                    {allFeedbacks.map(feedback =>
-                        <div className='flex mr-4 items-center' key={feedback.id}>
-                            <div
-                                className='flex flex-row mb-4 border-b-2 text-black w-full'
-                            >
-                                <p className='font-bold text-black min-w-24'>{feedback.naam}</p>
-                                <p>{feedback.opmerking}</p>
-                            </div>
+    return () => {
+      containerRef.current?.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-                            <LikeButton likes={feedback.likes} id={feedback.id} />
-                        </div>
-                    )}
-                </section>
-            </section>
+  const handleBackClick = () => {
+    if (containerRef.current) {
+      if (isMobile) {
+        containerRef.current.scrollBy({
+          left: -320,
+          behavior: "smooth",
+        });
+      } else {
+        containerRef.current.scrollBy({
+          left: -350,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  const handleNextClick = () => {
+    if (containerRef.current) {
+      if (isMobile) {
+        containerRef.current.scrollBy({
+          left: 320,
+          behavior: "smooth",
+        });
+      } else {
+        containerRef.current.scrollBy({
+          left: 350,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  const handleWheel = (event: any) => {
+    const div = containerRef.current;
+
+    if (
+      div &&
+      event.deltaX !== 0 &&
+      Math.abs(event.deltaX) > Math.abs(event.deltaY)
+    ) {
+      event.stopPropagation();
+    }
+  };
+
+  const btnStyling =
+    "p-2 pl-4 pr-4 border-gray-500 border-2 rounded-full w-10 h-10 flex justify-center items-center";
+
+  return (
+    <section className="min-h-screen bg-white pt-28 md:pl-40 pl-10 md:pr-40 pr-10 pb-28">
+      <section className="mb-4">
+        <h1 className="md:text-9xl lg:text-4xl text-2xl text-center md:mb-10 mb-6 text-black">
+          {title}
+        </h1>
+        <div className="wrapper flex justify-center mb-2">
+          <div
+            className="flex flex-row justify-start gap-10 overflow-x-scroll overflow-y-hidden"
+            style={{ width: `${80 * 17 + 10 * 3}px` }}
+            ref={containerRef}
+            onWheel={handleWheel}
+          >
+            {covers.map((image, index) => (
+              <Link key={index} href={image.title ?? ""}>
+                <div className="lg:w-36 xl:w-80 w-72">
+                  <Image
+                    src={image.url}
+                    alt={image.alt}
+                    height={image.height}
+                    width={image.width}
+                    priority
+                  />
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-end pr-4">
+          <button
+            className={`${btnStyling} mr-2 ${
+              scrollAtStart && "border-gray-100 text-gray-200"
+            }`}
+            onClick={handleBackClick}
+            disabled={scrollAtStart}
+          >
+            <p className="text-black">{`<`}</p>
+          </button>
+          <button
+            className={`${btnStyling} ${
+              scrollAtEnd && "border-gray-100 text-gray-200"
+            }`}
+            onClick={handleNextClick}
+            disabled={scrollAtEnd}
+          >
+            <p className="text-black">{`>`}</p>
+          </button>
+        </div>
+      </section>
+      <section className="lg:grid lg:grid-cols-5 xl:ml-40 xl:pr-40">
+        <section className="lg:col-span-2">
+          <article>
+            <Form />
+          </article>
         </section>
-    )
-}
+        <section className="lg:col-span-3 lg:grid overflow-y-scroll h-80">
+          {allFeedbacks.map((feedback) => (
+            <div className="flex gap-4 items-center" key={feedback.id}>
+              <div className="flex flex-row border-b-2 mb-2 pb-2 text-black w-full">
+                <p className="font-bold text-black min-w-24">{feedback.naam}</p>
+                <p>{feedback.opmerking}</p>
+              </div>
 
-export default IAM
+              <LikeButton likes={feedback.likes} id={feedback.id} />
+            </div>
+          ))}
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default IAM;
