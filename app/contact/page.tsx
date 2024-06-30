3f4f80c3d6eeb00d7db9b96cf68f9fa1fd82ac3a
Styn Vercauteren <styn.vercauteren@two-point-o.be>
2024-06-30 21:43:01 +0200

    smaller pic

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,49 +3,69 @@ import Icon from "@/components/Icon/Icon";
 import { performRequest } from "@/datocms/performRequest";
 import { CONTACT_PAGE_CONTENT_QUERY } from "@/datocms/queries";
 import { HomePagina } from "@/models/homePagina";
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Contact() {
-    const { data: { homePagina } } = await performRequest<{ data: { homePagina: HomePagina } }>({ query: CONTACT_PAGE_CONTENT_QUERY })
+  const {
+    data: { homePagina },
+  } = await performRequest<{ data: { homePagina: HomePagina } }>({
+    query: CONTACT_PAGE_CONTENT_QUERY,
+  });
 
-    return (
-        <>
-            <section
-                className={'flex flex-col items-center justify-center text-xl bg-white min-h-screen lg:pr-40 pr-10 lg:pl-40 pl-10'}
-            >
-                <Image
-                    src={homePagina.contactLogo.url}
-                    alt={homePagina.contactLogo.alt}
-                    height={homePagina.contactLogo.height}
-                    width={homePagina.contactLogo.width}
-                    priority
-                    className="w-3/4"
-                />
+  return (
+    <>
+      <section
+        className={
+          "flex flex-col items-center justify-center text-xl bg-white min-h-screen lg:pr-40 pr-10 lg:pl-40 pl-10"
+        }
+      >
+        <Image
+          src={homePagina.contactLogo.url}
+          alt={homePagina.contactLogo.alt}
+          height={homePagina.contactLogo.height}
+          width={homePagina.contactLogo.width}
+          priority
+          className="w-3/5"
+        />
 
-                <div className='flex flex-row gap-5 items-center'>
-                    <Icon href={homePagina.facebook} src={homePagina.facebookLogo.url} alt={homePagina.facebookLogo.alt} />
-                    <Icon href={homePagina.instagram} src={homePagina.instagramLogo.url} alt={homePagina.instagramLogo.alt} />
-                    <Icon href={homePagina.linkedin} src={homePagina.linkedinLogo.url} alt={homePagina.linkedinLogo.alt} />
-                </div>
+        <div className="flex flex-row gap-5 items-center">
+          <Icon
+            href={homePagina.facebook}
+            src={homePagina.facebookLogo.url}
+            alt={homePagina.facebookLogo.alt}
+          />
+          <Icon
+            href={homePagina.instagram}
+            src={homePagina.instagramLogo.url}
+            alt={homePagina.instagramLogo.alt}
+          />
+          <Icon
+            href={homePagina.linkedin}
+            src={homePagina.linkedinLogo.url}
+            alt={homePagina.linkedinLogo.alt}
+          />
+        </div>
 
-                <div className='flex flex-col items-center pt-10'>
-                    <Link
-                        href={`mailto:${homePagina.email}`}
-                        className='text-xl text-black'
-                    >
-                        {homePagina.email}
-                    </Link>
-                    <div
-                        className='text-center text-xl text-black'
-                    >
-                        <p>{homePagina.straat} {homePagina.huisnummer}</p>
-                        <p>{homePagina.gemeente} {homePagina.postcode}</p>
-                    </div>
-                </div>
-            </section>
+        <div className="flex flex-col items-center pt-10">
+          <Link
+            href={`mailto:${homePagina.email}`}
+            className="text-xl text-black"
+          >
+            {homePagina.email}
+          </Link>
+          <div className="text-center text-xl text-black">
+            <p>
+              {homePagina.straat} {homePagina.huisnummer}
+            </p>
+            <p>
+              {homePagina.gemeente} {homePagina.postcode}
+            </p>
+          </div>
+        </div>
+      </section>
 
-            <Footer />
-        </>
-    )
+      <Footer />
+    </>
+  );
 }
