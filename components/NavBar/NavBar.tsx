bc2e3332573e7f801e1bb7e8a30259eb56c280e7
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-07-21 11:23:20 +0200

    update navbar with fotos page

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -13,6 +13,9 @@ const NavBar = () => (
                 <Link href='/kalenders'>Galerijen</Link>
             </li>
             <li className='py-2 px-3 text-black md:text-2xl hover:text-blue-700 cursor-pointer'>
+                <Link href='/fotos'>{`Foto's`}</Link>
+            </li>
+            <li className='py-2 px-3 text-black md:text-2xl hover:text-blue-700 cursor-pointer'>
                 <Link href='/contact'>Contact</Link>
             </li>
         </ul>
