fc490f748e3ae0fc353c2b802167ae73ff1a74d7
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-12-03 14:13:51 +0100

    fix build

--- app/kalenders/page.tsx
+++ app/kalenders/page.tsx
@@ -1,8 +1,7 @@
 import Kalenders from '@/components/Pages/Kalenders'
 import { performRequest } from '@/datocms/performRequest'
-import { KALENDER_CONTENT_QUERY, ONLY_LOGO_QUERY } from '@/datocms/queries'
+import { KALENDER_CONTENT_QUERY } from '@/datocms/queries'
 import { AllKalenders } from '@/models/allKalenders'
-import { HomePagina } from '@/models/homePagina'
 
 export default async function KalendersPage() {
   const {
@@ -11,22 +10,5 @@ export default async function KalendersPage() {
     query: KALENDER_CONTENT_QUERY,
   })
 
-  const {
-    data: { homePagina },
-  } = await performRequest<{ data: { homePagina: HomePagina } }>({
-    query: ONLY_LOGO_QUERY,
-  })
-
-  return (
-    <Kalenders
-      allKalenders={allKalenders}
-      logo={homePagina.logo}
-      facebook={homePagina.facebook}
-      facebookLogo={homePagina.facebookLogo}
-      instagram={homePagina.instagram}
-      instagramLogo={homePagina.instagramLogo}
-      linkedin={homePagina.linkedin}
-      linkedinLogo={homePagina.linkedinLogo}
-    />
-  )
+  return <Kalenders allKalenders={allKalenders} />
 }
--- components/Pages/Kalenders.tsx
+++ components/Pages/Kalenders.tsx
@@ -6,29 +6,8 @@ import KalenderItem from '@/components/KalenderItem/KalenderItem'
 import getEmptyKalender from '@/helpers/getEmptyKalender'
 import shuffleArray from '@/helpers/shuffleArray'
 import { AllKalenders } from '@/models/allKalenders'
-import { Image } from '@/models/image'
 
-import Footer from '../Footer/Footer'
-
-const KalendersPage = ({
-  allKalenders,
-  logo,
-  facebook,
-  facebookLogo,
-  instagram,
-  instagramLogo,
-  linkedin,
-  linkedinLogo,
-}: {
-  allKalenders: AllKalenders[]
-  logo: Image
-  facebook: string
-  facebookLogo: Image
-  instagram: string
-  instagramLogo: Image
-  linkedin: string
-  linkedinLogo: Image
-}) => {
+const KalendersPage = ({ allKalenders }: { allKalenders: AllKalenders[] }) => {
   const [kalenders, setKalenders] = useState(() => {
     const randomSpots = new Array(6).fill(null).map(getEmptyKalender)
     const combined = allKalenders.concat(randomSpots)
@@ -36,27 +15,15 @@ const KalendersPage = ({
   })
 
   return (
-    <>
-      <section
-        className={
-          'bg-white min-h-screen grid grid-cols-4 grid-rows-3 gap-5 h-screen pt-40 lg:pr-40 pr-10 pb-20 lg:pl-40 pl-10'
-        }
-      >
-        {kalenders.map((kalenderItem, index) => (
-          <KalenderItem kalenderItem={kalenderItem} key={index} />
-        ))}
-      </section>
-      <Footer
-        fixed={false}
-        logo={logo}
-        facebook={facebook}
-        facebookLogo={facebookLogo}
-        instagram={instagram}
-        instagramLogo={instagramLogo}
-        linkedin={linkedin}
-        linkedinLogo={linkedinLogo}
-      />
-    </>
+    <section
+      className={
+        'mt-auto grid grid-cols-4 grid-rows-3 gap-5 h-screen pt-40 lg:pr-40 pr-10 pb-20 lg:pl-40 pl-10'
+      }
+    >
+      {kalenders.map((kalenderItem, index) => (
+        <KalenderItem kalenderItem={kalenderItem} key={index} />
+      ))}
+    </section>
   )
 }
 
